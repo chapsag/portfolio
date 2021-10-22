19d8d3cd74b689a8f5a6b82f11e085844751abd2
chapsag <chapsag@gmail.com>
2021-10-22 17:38:43 -0400

    Update font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
+import Fonts from '../components/font'
 
 // Component is the page view that will be rendered
 // pageProps is props that each page will receive when rendered
 const Portfolio = ({ Component, pageProps, router }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
