f97d2e1eda35a80392866a038e515dc517fef422
chapsag <chapsag@gmail.com>
2021-10-24 17:10:44 -0400

    Bugfix: fix type into section

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,19 +1,26 @@
 import { motion } from 'framer-motion'
 import { chakra, shouldForwardProp } from '@chakra-ui/react'
 
+type sectionProps = {
+  children: React.ReactNode
+  delay?: number
+}
+
 const StyledDiv = chakra(motion.div, {
   shouldForwardProp: props => {
     return shouldForwardProp(props) || props === 'transition'
   }
 })
 
-const Section = ({ children, delay = 0 }) => {
+const Section = ({ children, delay = 0 }: sectionProps) => {
   return (
     <StyledDiv
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       mb={6}
-      transition={{ duration: 0.8, delay }}
+      transition={
+        { duration: 0.8, delay } as any /* not the best way to do this */
+      }
     >
       {children}
     </StyledDiv>
