b7978f35d06bc058bef2107392b549849ef598f9
chapsag <chapsag@gmail.com>
2021-10-25 13:52:55 -0400

    update style for logo box

--- src/components/logo/styles.ts
+++ src/components/logo/styles.ts
@@ -1,15 +1,11 @@
 import styled from '@emotion/styled'
 
 export const LogoBox = styled.span`
-font-weight: bold;
-font-size: 18px;
-display: inline-flex;
-align-items: center;
-height: 30px;
-line-height: 20px;
-padding: 10px;
-
-&:hover img {
-    transform: rotate:(20deg);
-}
+  font-weight: bold;
+  font-size: 24px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
 `
