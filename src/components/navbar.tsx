c4d8f71c2878e685b5ae4917951504283f02fefa
chapsag <chapsag@gmail.com>
2021-10-26 15:52:57 -0400

    Bugfix: light mode navbar color

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -42,7 +42,7 @@ const LinkItem = ({
   ...props
 }: LinkItemProps) => {
   const active = path === href
-  const inactiveColor = useColorModeValue('gray.200', 'whiteAlpha.900')
+  const inactiveColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
   return (
     <NextLink href={href} passHref>
       <Link
