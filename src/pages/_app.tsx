add64daf79a640d776ddf8bcdf4d4cb1817cf488
chapsag <chapsag@gmail.com>
2021-12-02 13:41:18 -0500

    add GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,18 @@ import Layout from '../components/layouts/main'
 import { AnimatePresence } from 'framer-motion'
 import theme from '../lib/theme'
 import Fonts from '../lib/fonts'
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 
 const Portfolio = ({
   Component,
   pageProps,
   router
 }: AppProps): ReactElement => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-N3WVPBB' })
+  }, [])
+
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
